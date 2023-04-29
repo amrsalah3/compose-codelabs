fd5687bf16a9467e7ce9ce46923b425b834a42e1
Amr Salaheddin Abdelhady <amrhady3@gmail.com>
2023-04-29 08:31:09 +0300

    Initial commit

---
+++ app/src/main/java/com/narify/basicstatecodelab/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.narify.basicstatecodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.narify.basicstatecodelab.ui.theme.BasicStateCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicStateCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    WellnessScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        val list = remember { wellnessViewModel.tasks }
+        WellnessTasksList(
+            list = list,
+            onCheckedTask = { task, checked -> wellnessViewModel.changeTaskChecked(task, checked) },
+            onCloseTask = { task -> wellnessViewModel.remove(task) }
+        )
+    }
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+
+    StatelessCounter(count, { count++ }, modifier)
+}
+
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320
+)
+@Composable
+fun WellnessScreenPreview() {
+    BasicStateCodelabTheme {
+        WellnessScreen()
+    }
+}
---
+++ app/src/main/java/com/narify/basicstatecodelab/WellnessTaskItem.kt
@@ -0,0 +1,46 @@
+package com.narify.basicstatecodelab
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.narify.basicstatecodelab.ui.theme.BasicStateCodelabTheme
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            modifier = modifier
+                .weight(1f)
+                .padding(start = 16.dp),
+            text = taskName
+        )
+        Checkbox(checked, onCheckedChange)
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WellnessTaskItemPreview() {
+    BasicStateCodelabTheme {
+        WellnessTaskItem("Task Item 1", true, {}, {})
+    }
+}
